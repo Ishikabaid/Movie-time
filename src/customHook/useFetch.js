4e184310977c2a3652e1f059b9990c743954d621
Ishikabaid <ibishikabaid@gmail.com>
2022-09-21 01:02:57 +0530

    fixed api

--- src/customHook/useFetch.js
+++ src/customHook/useFetch.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export const BASE_API_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+export const BASE_API_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 
 const useFetch = (apiParams) => {
     const [isLoading, setIsLoading] = useState(true);
